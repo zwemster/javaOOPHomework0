5dc9c254daa11dde07f1f2e40f2e004ba978900f
maxim_stoma <maksim.stoma@mail.ru>
2023-03-04 02:44:50 +0300

    updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VendingMachine machine = new VendingMachine();
+        machine.addProduct(new Product("Lay's", 99.50));
+        machine.addProduct(new Product("Bounty", 70.30));
+        machine.addProduct(new Product("Sneakers", 85.15));
+        machine.addProduct(new Product("KitKat", 77.60));
+        machine.addProduct(new Product("Nesquik", 35.95));
+        machine.addProduct(new PerisableProduct("Молоко Бурёнка", 89.50, new Date(2023, 1, 25)));
+        machine.addProduct(new PerisableProduct("Молоко Коровка", 93.20, new Date(2023, 1, 25)));
+        machine.addProduct(new PerisableProduct("Молоко Василёк", 91.10, new Date(2023, 1, 25)));
+
+        System.out.println(machine);
     }
 }
